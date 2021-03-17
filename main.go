bd209a769f554cfefdea185d507194ad7fd5c53e
ghifarit53 <59758342+ghifarit53@users.noreply.github.com>
2021-03-17 08:29:35 +0700

    some *rough* fix, ajax call on click

--- main.go
+++ main.go
@@ -16,24 +16,51 @@ func handlerIndex(w http.ResponseWriter, r *http.Request) {
 }
 
 func playBellRinging(w http.ResponseWriter, r *http.Request) {
-	exec.Command("mpv", "./static/audio/bell-ringing.mp3").Run()
+	// very rough, and everything is hardcoded
+	// but this is the best I could do
+	exec.Command("pkill", "mpv").Run()
+	exec.Command("mpv", "--no-video", "--loop-playlist=no", "./static/audio/bell-ringing.mp3").Run()
+}
+
+func playAnnoStart(w http.ResponseWriter, r *http.Request) {
+	exec.Command("pkill", "mpv").Run()
+	exec.Command("mpv", "--no-video", "--loop-playlist=no", "./static/audio/anno-start.mp3").Run()
+}
+
+func playAnnoStop(w http.ResponseWriter, r *http.Request) {
+	exec.Command("pkill", "mpv").Run()
+	exec.Command("mpv", "--no-video", "--loop-playlist=no", "./static/audio/anno-stop.mp3").Run()
 }
 
 func playDangerSiren(w http.ResponseWriter, r *http.Request) {
-	exec.Command("mpv", "./static/audio/danger-siren.mp3").Run()
+	exec.Command("pkill", "mpv").Run()
+	exec.Command("mpv", "--no-video", "--loop-playlist=no", "./static/audio/danger-siren.mp3").Run()
 }
 
 func playEarthquakeSiren(w http.ResponseWriter, r *http.Request) {
-	exec.Command("mpv", "./static/audio/earthquake-siren.mp3").Run()
+	exec.Command("pkill", "mpv").Run()
+	exec.Command("mpv", "--no-video", "--loop-playlist=no", "./static/audio/earthquake-siren.mp3").Run()
+}
+
+func stopAudio(w http.ResponseWriter, r *http.Request) {
+	exec.Command("pkill", "mpv").Run()
 }
 
 func main() {
 	// routing
 	http.HandleFunc("/", handlerIndex)
+
+	// regular bell
 	http.HandleFunc("/bell-ringing", playBellRinging)
+	http.HandleFunc("/anno-start", playAnnoStart)
+	http.HandleFunc("/anno-stop", playAnnoStop)
+
+	// danger siren
 	http.HandleFunc("/danger-siren", playDangerSiren)
 	http.HandleFunc("/earthquake-siren", playEarthquakeSiren)
+	http.HandleFunc("/stop-audio", stopAudio)
 
+	// TODO: use port from argv
 	var address = "localhost:1337"
 	fmt.Printf("server started at %s\n", address)
 	err := http.ListenAndServe(address, nil)
